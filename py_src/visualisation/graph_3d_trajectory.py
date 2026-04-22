d935a7d6fed1c198157903c9c8513ccb075f3a2b
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-04-22 17:15:31 +1200

    feat: plot points on curve

--- py_src/visualisation/graph_3d_trajectory.py
+++ py_src/visualisation/graph_3d_trajectory.py
@@ -7,45 +7,26 @@ import matplotlib.pyplot as plt
 def show_trajectory():
     ax = plt.figure().add_subplot(projection='3d')
 
-    # t = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-    # t = np.linspace(0, 10, 100)
-    # tx = np.array((np.pi / 2), dtype=np.float64)
-    # tx = np.linspace(0, 10, 100)
-    # ty = np.array((np.pi / 2), dtype=np.float64)
-    # ty = np.linspace(10, 0, 100)
-    # tz = np.linspace(10, 0, 10)
-
-    # tx = np.linspace(0, 10, 10)
-    # ty = tx**2
-    # tz = tx**3
-
-    t = np.linspace(0, 2*np.pi, 32)  # Nodes
+    node_count = 32
+    t = np.linspace(0, 2*np.pi, node_count)  # Nodes
     tx = 2 * np.cos(t)
     ty = 2 * np.sin(t)
     tz = (10/5) - (2/5)*(2 * np.cos(t)) - (3/5)*(2 * np.sin(t))
 
     draw_parametric_function(ax, tx, ty, tz, 'foot trajectory')
+    plot_points(ax, tx, ty, tz)
 
     ax.legend()
     plt.show()
 
 def draw_parametric_function(ax, tx:npt.NDArray[np.float64], ty:npt.NDArray[np.float64], tz:npt.NDArray[np.float64], label:str):
 
-    # Prepare arrays x, y, z
-    # theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-    # theta = t
-    # z = np.linspace(-2, 2, 10)
-    # # r = z**2 + 1
-    # x = tx * np.sin(tx)
-    # y = ty * np.cos(ty)
-    x = tx
-    y = ty
-    z = tz
-
-    get_points(x, y, z)
-
     # Draw parametric curve
-    ax.plot(x, y, z, label=label)
+    ax.plot(tx, ty, tz, label=label)
+
+def plot_points(ax, x:npt.NDArray[np.float64], y:npt.NDArray[np.float64], z:npt.NDArray[np.float64]):
+    points = get_points(x, y, z)
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='r', label='Points')
 
 def get_points(x:npt.NDArray[np.float64], y:npt.NDArray[np.float64], z:npt.NDArray[np.float64]):
     max_len = max(x.size, y.size, z.size)
@@ -57,3 +38,5 @@ def get_points(x:npt.NDArray[np.float64], y:npt.NDArray[np.float64], z:npt.NDArr
     points = np.column_stack((x_repeated, y_repeated, z_repeated))
     print(repr(points))
     print(f"{len(points)} points")
+    
+    return points
