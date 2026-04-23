3904eeeb131b706aa80d2f448ac92eba1f24b065
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-04-23 17:22:23 +1200

    docs: add return statements and improve clarity

--- py_src/visualisation/lagrange_interpolator.py
+++ py_src/visualisation/lagrange_interpolator.py
@@ -5,9 +5,15 @@ from typing import Annotated
 type Point3D = Annotated[npt.NDArray[np.float64], (3,)]  # (A Coordinate) 1D array with 3 elements
 type PointList = Annotated[npt.NDArray[np.float64], (None, 3)]  # 2D array of points/coordinates
 
-def lagrange_basis(i:int, t:np.float64|float, t_values:npt.NDArray[np.float64]) -> float:
+def lagrange_basis(i:int, t:float|np.float64, t_values:npt.NDArray[np.float64]) -> float:
     """
     Compute the i-th Lagrange basis polynomial weight at time t.
+
+    Note:
+        Assumes t_values are unique to avoid division by zero.
+    
+    Returns:
+        (float): The influence (Lagrange basis polynomial weight) the i-th control point has on the curve at time t.
     
     Args:
         i: The control point index we are calculating weight for.
@@ -22,22 +28,21 @@ def lagrange_basis(i:int, t:np.float64|float, t_values:npt.NDArray[np.float64]) 
     
     return basis
 
-def lagrange_interpolate(t:np.float64|float, t_values:npt.NDArray[np.float64], control_points:PointList) -> Point3D:
+def lagrange_interpolate(t:float|np.float64, t_values:npt.NDArray[np.float64], control_points:PointList) -> Point3D:        
     """
     Interpolates a single point at time t.
+
+    Returns:
+        Point3D: The interpolated point at time t.
     
     Args:
         t: The time-step we are evaluating.
         t_values: The array of time-anchors for each control point.
         control_points: The array of control points.
     """
-    x, y, z = 0.0, 0.0, 0.0
+    # Generate weights for all control points
+    #   the influence each control point has on the curve (at time t)
+    weights = np.array([lagrange_basis(i, t, t_values) for i in range(len(control_points))])
 
-    # Combine lagrange polynomials
-    for i in range(len(control_points)):
-        weight = lagrange_basis(i, t, t_values)
-        x += control_points[i, 0] * weight
-        y += control_points[i, 1] * weight
-        z += control_points[i, 2] * weight
-    
-    return np.array([x, y, z])
+    # Calculate point based on weights, using dot product
+    return np.dot(weights, control_points)
