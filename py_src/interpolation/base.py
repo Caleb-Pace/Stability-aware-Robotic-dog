1a02f3aa2703edb22480c2fa3157e6eddbe84d71
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-04 17:38:00 +1200

    feat: add & improve safety checks

--- py_src/interpolation/base.py
+++ py_src/interpolation/base.py
@@ -33,25 +33,31 @@ class Interpolator(ABC):
         pass
 
     def compute_interpolated_points(self, control_points:PointList, node_count:int, maximum_time:float|None=None) -> PointList:
-        if node_count <= 0:
-            raise ValueError("node_count must be positive")
-        if node_count < 2:
-            raise IndexError(f"Not enough nodes to interpolate! ({len(control_points)})")
         if len(control_points) < 2:
-            raise IndexError(f"Not enough control points to interpolate! ({len(control_points)})")
+            raise IndexError(f"Not enough control points to interpolate! ({len(control_points)} >= 2)")
+        
+        if node_count <= 0:
+            raise ValueError(f"{{node_count}} must be positive! ({node_count} > 0)")
+
 
         t_anchors = self.calculate_time_anchors(control_points)
-        if len(control_points) != len(t_anchors):  # Sanity check
-            raise IndexError(f"Bad {{Interpolator.calculate_time_anchors}} method! (Length mismatch between {{control_points}} and {{t_anchors}}! {len(control_points)} != {len(t_anchors)})")
+        if not np.all(np.diff(t_anchors) > 0):     # Sanity check - Strictly increasing (no duplicates)
+            raise ValueError(f"Bad {{{self.__class__.__name__}.calculate_time_anchors}} method!\n\t{{t_anchors}} must be strictly increasing.")
+        if t_anchors[0] != 0:                      # Sanity check - Starts at 0
+            raise ValueError(f"Bad {{{self.__class__.__name__}.calculate_time_anchors}} method!\n\tCurve origin/{{t_anchors[0]}} must be zero! ({t_anchors[0]} == 0)")
+        if len(control_points) != len(t_anchors):  # Sanity check - Corresponds to control points
+            raise IndexError(f"Bad {{{self.__class__.__name__}.calculate_time_anchors}} method!\n\tLength mismatch between {{control_points}} and {{t_anchors}}. ({len(control_points)} == {len(t_anchors)})")
+
 
         t_samples = np.linspace(0, (maximum_time or t_anchors[-1]), node_count)
     
         # Remove out of range time samples (samples that are not on the curve)
-        if not maximum_time:  
+        if not maximum_time:
             t_samples = t_samples[(t_samples >= t_anchors[0]) & (t_samples <= t_anchors[-1])]
 
-        if len(t_samples) < 2:  # Sanity check
-            raise IndexError(f"Not enough time samples to interpolate! ({len(t_samples)})")
+        if len(t_samples) <= 0:  # Sanity check
+            raise IndexError(f"Not enough time samples to interpolate! ({len(t_samples)} > 0)")
+
 
         interpolated_points = np.empty((len(t_samples), 3))  # Pre-allocate PointList (2D array)
 
