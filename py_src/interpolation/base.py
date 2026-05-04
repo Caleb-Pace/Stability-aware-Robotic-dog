96502aaf165be8e44de5fa3c207438622d54976e
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-04 16:35:52 +1200

    feat: add method to calculate interpolated points

--- py_src/interpolation/base.py
+++ py_src/interpolation/base.py
@@ -31,3 +31,24 @@ class Interpolator(ABC):
             control_points: The array of control points.
         """
         pass
+
+    def compute_interpolated_points(self, control_points:PointList, node_count:int, maximum_time:float|None=None) -> PointList:
+        if node_count <= 0:
+            raise ValueError("node_count must be positive")
+        if len(control_points) < 2:
+            raise IndexError(f"Not enough control points to interpolate! ({len(control_points)})")
+
+        t_anchors = self.calculate_time_anchors(control_points)
+        t_samples = np.linspace(0, (maximum_time or t_anchors[-1]), node_count)
+    
+        interpolated_points = np.empty((node_count, 3))  # Pre-allocate 2D array
+
+        # Calculate interpolated points
+        for i, t in enumerate(t_samples):
+            if t > t_anchors[-1]:  # Sample not in range / on curve
+                interpolated_points = interpolated_points[:i]  # Resize array to valid points
+                break
+            
+            interpolated_points[i] = self.interpolate_point(t, t_anchors, control_points)
+
+        return interpolated_points
