7109f8c7f4012de6a5d66c58727ff00ee61e49f7
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-14 10:40:15 +1200

    refactor(gait_definition): change types and rename variables

--- py_src/kinematic_controller/gait_definition.py
+++ py_src/kinematic_controller/gait_definition.py
@@ -1,16 +1,13 @@
 import numpy as np
 import numpy.typing as npt
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Gait:
-    body_height: float
+    body_height: npt.NDArray[np.float64]  # (Height)
     # body_pitch: float  # Future
     # body_roll: float  # Future
 
-    leg_phases_offset: npt.NDArray[np.float64]  # TODO: Could rename to leg_sequence
-    leg_duty_factor: npt.NDArray[np.float64]
-    duty_cycle_length: float
-    
-    # leg_start_position: np.ndarray = field(default_factory=lambda: np.zeros((4, 2), dtype=float))
-    # leg_end_position: np.ndarray = field(default_factory=lambda: np.zeros((4, 2), dtype=float))
+    leg_phase_offset:   npt.NDArray[np.float64]  # (Time start offset)
+    leg_control_points: npt.NDArray[np.float64]  # (Leg, Control Point, Coordinate) | Relative Coordinates
+    absolute_time:      float
--- py_src/kinematic_controller/gaits.py
+++ py_src/kinematic_controller/gaits.py
@@ -4,8 +4,13 @@ from kinematic_controller.gait_definition import Gait
 LEG_COUNT:int = 4
 
 TROT = Gait(
-    leg_phases_offset = np.array([[0.0], [0.5], [0.5], [0.0]], dtype=float),
-    leg_duty_factor   = np.array([[0.5], [0.5], [0.5], [0.5]], dtype=float),
-    duty_cycle_length = 0.8,
-    body_height = 0.5
+    leg_phase_offset   = np.array([[0.0], [0.5], [0.5], [0.0]], dtype=float),
+    leg_control_points = np.array([
+        [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
+        [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
+        [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
+        [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]]
+    ], dtype=float),
+    absolute_time      = 12.0,
+    body_height        = np.array([[0.5]], dtype=float)
 )
