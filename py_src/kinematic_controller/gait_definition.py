0264c27c2e5aa79792faf59d981e550aa48a6023
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-14 11:53:34 +1200

    refactor: make gait definition standard class instead of dataclass to pre-compute values

--- py_src/kinematic_controller/gait_definition.py
+++ py_src/kinematic_controller/gait_definition.py
@@ -1,8 +1,6 @@
 import numpy as np
 import numpy.typing as npt
-from dataclasses import dataclass
 
-@dataclass(frozen=True)
 class Gait:
     body_height: npt.NDArray[np.float64]  # (Height)
     # body_pitch: float  # Future
@@ -11,3 +9,4 @@ class Gait:
     leg_phase_offset:   npt.NDArray[np.float64]  # (Time start offset)
     leg_control_points: npt.NDArray[np.float64]  # (Leg, Control Point, Coordinate) | Relative Coordinates
     absolute_time:      float
+    
