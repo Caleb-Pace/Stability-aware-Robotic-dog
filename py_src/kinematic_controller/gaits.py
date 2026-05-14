62a6a31eabdace935961e9dc8766a52f28f8f3f7
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-14 17:14:03 +1200

    fix: correct arrays

--- py_src/kinematic_controller/gaits.py
+++ py_src/kinematic_controller/gaits.py
@@ -3,12 +3,12 @@ from kinematic_controller.gait_definition import Gait
 
 TROT = Gait(
     node_count = 32,
-    leg_phase_offset   = np.array([[0.0], [0.5], [0.5], [0.0]], dtype=float),
+    leg_phase_offset   = np.array([0.0, 0.5, 0.5, 0.0], dtype=float),
     leg_control_points = np.array([
         [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
         [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
         [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]],
         [[-5.0, 0.0, 0.0], [-2.5, 5.0, 0.0], [0.0, 7.5, 0.0], [2.5, 5.0, 0.0], [5.0, 0.0, 0.0]]
     ], dtype=float),
-    body_height        = np.array([[0.5]], dtype=float)
+    body_height        = np.array([0.5], dtype=float)
 )
