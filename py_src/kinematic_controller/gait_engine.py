10ea3f939705640dd26708c4289e4a87285168c7
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-19 22:20:22 +1200

    wip-feat: add basic gait engine implementation

--- py_src/kinematic_controller/gait_engine.py
+++ py_src/kinematic_controller/gait_engine.py
@@ -1,10 +1,29 @@
+import numpy as np
 from kinematic_controller.gait_definition import Gait
+from kinematic_controller.output import RobotOutput
 from kinematic_controller.stepper import step
 
 
 class GaitEngine:
     def __init__(self, gait:Gait, output:RobotOutput):
-        pass
+        self.step = 0
+        self.gait = gait
+        self.output = output  # Hardware abstraction
 
-    def input(self, pos_delta_x:float):
-        pass
+    def input(self, delta_x:float):
+        if delta_x > 1:
+            self.step += 1
+        if delta_x < 1:
+            self.step -= 1
+
+        self._update()
+
+    def _update(self):
+        self.step %= self.gait.steps_in_gait
+        foot_positions = step(self.gait, self.step)
+
+        # TODO: Add IK SOLVER
+        motor_angles = []
+        feedforward_torques = np.zeros(12)  # TODO: Calculate Torques properly
+
+        self.output.send_commands(motor_angles, feedforward_torques)
