c5de25a46a00c007722a08b727c44c126c1645d3
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-19 22:27:51 +1200

    wip-feat: add step delay

--- py_src/kinematic_controller/gait_engine.py
+++ py_src/kinematic_controller/gait_engine.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from kinematic_controller.gait_definition import Gait
 from kinematic_controller.output import RobotOutput
@@ -6,19 +7,26 @@ from kinematic_controller.stepper import step
 
 class GaitEngine:
     def __init__(self, gait:Gait, output:RobotOutput):
-        self.step = 0
+        self.step:int = 0
+        self.delay_ms:float = 1
+        
         self.gait = gait
         self.output = output  # Hardware abstraction
 
-    def input(self, delta_x:float):
+    def input(self, delta_x:float) -> None:
         if delta_x > 1:
             self.step += 1
         if delta_x < 1:
             self.step -= 1
-
+        
         self._update()
 
-    def _update(self):
+        # Crude step delay implementation
+        self.delay_ms = 2 * (1 - abs(delta_x))  # [0, 2] ms delay
+        time.sleep(self.delay_ms)
+
+    # TODO: Improve update (calling) system
+    def _update(self) -> None:
         self.step %= self.gait.steps_in_gait
         foot_positions = step(self.gait, self.step)
 
