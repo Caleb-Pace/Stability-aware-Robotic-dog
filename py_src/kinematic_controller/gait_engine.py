b29715dee0481ecf00a5b4e24faed33a0359e548
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-05-19 22:05:17 +1200

    chore: define prototypes

--- py_src/kinematic_controller/gait_engine.py
+++ py_src/kinematic_controller/gait_engine.py
@@ -1,4 +1,10 @@
+from kinematic_controller.gait_definition import Gait
+from kinematic_controller.stepper import step
 
 
 class GaitEngine:
-    pass
+    def __init__(self, gait:Gait, output:RobotOutput):
+        pass
+
+    def input(self, pos_delta_x:float):
+        pass
