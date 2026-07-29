e253133bf1f69dfb797652bb5d0f561540d24b59
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-07-29 19:40:01 +1200

    fix: unpack target angles for checks and printing

--- py_src/hardware_abstraction_layer/dummy_out.py
+++ py_src/hardware_abstraction_layer/dummy_out.py
@@ -8,20 +8,24 @@ class DummyOutput(RobotOutput):
         pass
 
     def send_commands(self, target_angles, torques):
-        if len(target_angles) < 12:
-            print(f"ERROR: not enough angles ({len(target_angles)} == 12)(from Dummy Output)")
+        if len(target_angles) < 4:
+            print(f"ERROR: not enough leg angles ({len(target_angles)} == 4)(from Dummy Output)")
             return
-        if len(target_angles) > 12:
-            print(f"ERROR: too many angles ({len(target_angles)} == 12)(from Dummy Output)")
+        if len(target_angles) > 4:
+            print(f"ERROR: too many leg angles ({len(target_angles)} == 4)(from Dummy Output)")
             return
-        if len(target_angles) != len(torques):
+        if len(target_angles.ravel()) != len(torques):
             print("ERROR: angle-torque mismatch (from Dummy Output)")
             return
 
         print(f"Dummy Output - Recieved Instructions:")
         leg_num:int = 0
-        for angle, torque in zip(target_angles, torques):
-            print(f"  Leg[{leg_num}] {{angle target: {np.round(np.degrees(angle), 3):>6}° }}; {{torque: {np.round(torque, 5):>8} Nm }}")
+        for angle, torque in zip(target_angles.ravel(), torques):
+            if leg_num % 3 == 0:
+                print(f"  Leg[{(leg_num // 3)}]:")
+
+            print(f"         Motor[{leg_num}] {{angle target: {np.round(np.degrees(angle), 3):>7}° }}; {{torque: {np.round(torque, 5):>8} Nm }}")
+
             leg_num += 1
     
     def get_low_state(self):
