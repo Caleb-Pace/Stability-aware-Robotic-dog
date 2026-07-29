8685661f6bf8b6342b779dda24ce6a847f2d3e0c
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-07-29 17:36:16 +1200

    feat: add input test script to main

--- py_src/kinematic_controller/main.py
+++ py_src/kinematic_controller/main.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
-import math
+import time
+import numpy as np
 from kinematic_controller.gaits import TROT
 from kinematic_controller.gait_definition import Gait
 from kinematic_controller.stepper import step
 from kinematic_controller.ik_solver import IK_Solver
+from hardware_abstraction_layer import InputLayer, GamepadController
 
 def main():
     gait:Gait = TROT
+    gait_detail = gait.steps_in_gait  # (Point/Node count)
 
-    print(f"#L1_points: {len(gait.time_anchors[0])}; Gait_steps: {gait.steps_in_gait}")  # TODO: Remove, for debugging
-    detail = gait.steps_in_gait  # (Point/Node count)
+    in_layer:InputLayer = GamepadController()
+    read_delay_ms = 100
 
-    for i in range(0, detail):
-        foot_positions = step(gait, i)
-        print(f"{repr(foot_positions)}")
 
-        # Get motor angles
-        ik = IK_Solver()
-        result = ik.solve(foot_positions)
-        if result is None:
-            return None
-        print(f"    {repr(result)}")
+    while True:
+        input_data = in_layer.poll()
+
+        input_sum = abs(input_data.left_stick.delta_x) + abs(input_data.left_stick.delta_y) + abs(input_data.right_stick.delta_x) + abs(input_data.right_stick.delta_y)
+        has_input = input_sum > 0
+        if has_input:
+            print(f"    L ({np.round(input_data.left_stick.delta_x, 3):>6}, {np.round(input_data.left_stick.delta_y, 3):>6})    |    R ({np.round(input_data.right_stick.delta_x, 3):>6}, {np.round(input_data.right_stick.delta_y, 3):>6})")
+
+        time.sleep(read_delay_ms / 1000)
+
+    # print(f"#L1_points: {len(gait.time_anchors[0])}; Gait_steps: {gait.steps_in_gait}")  # TODO: Remove, for debugging
+
+    # for i in range(0, gait_detail):
+    #     foot_positions = step(gait, i)
+    #     print(f"{repr(foot_positions)}")
+
+    #     # Get motor angles
+    #     ik = IK_Solver()
+    #     result = ik.solve(foot_positions)
+    #     if result is None:
+    #         return None
+    #     print(f"    {repr(result)}")
 
 
 if __name__ == "__main__":
