00318cdf429f0d9aa9d5d1afedb1ac66f9925999
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-07-29 15:10:53 +1200

    feat: block out gait engine design

--- py_src/kinematic_controller/gait_engine.py
+++ py_src/kinematic_controller/gait_engine.py
@@ -7,13 +7,40 @@ from kinematic_controller.stepper import step
 
 class GaitEngine:
     def __init__(self, gait:Gait, output:RobotOutput):
-        self.step:int = 0
+        self.step_num:int = 0
         self.delay_ms:float = 1
         
         self.gait = gait
         self.output = output  # Hardware abstraction
 
-    def input(self, delta_x:float) -> None:
+    def clock_tick(self) -> None:
+        # self._dynamic_recovery
+        # if {recovery} then ignore other calls
+
+        # self._step
+        # self._pid
+        # combine PID and step results
+        pass
+
+    def clock_start(self, interval_ms:float) -> None:
+        self.clock_tick()
+        time.sleep(interval_ms)
+        self.clock_start(interval_ms)
+
+    def _pid(self, expected, actual) -> None:
+        # TODO: Call Maanas' implementation
+        pass
+
+    def _step(self):
+        pass
+
+    # _dynamic_recovery
+
+    def input(self) -> None:
+        pass
+
+
+    def input2(self, delta_x:float) -> None:
         if delta_x > 1:
             self.step += 1
         if delta_x < 1:
@@ -35,3 +62,17 @@ class GaitEngine:
         feedforward_torques = np.zeros(12)  # TODO: Calculate Torques properly
 
         self.output.send_commands(motor_angles, feedforward_torques)
+
+    def _output(self) -> None:
+        pass
+
+    # TODO: Later, dynamics model
+    def _get_feedforward_torques(self):
+        pass
+    def _dynamic_recovery(self): # -> {Result}|None:
+        # is_falling = { Forward Dynamics Solve }
+        # if is_falling:  # Save from fall
+        #     correction_forces = { Inverse Dynamics Solve }
+        #     TODO: Calculate safe/stable position
+        #     TODO: You are working in a time frame not instantly moving with those forces
+        pass
