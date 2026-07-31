94e4ff838890d76927a6f8ecfc153cfa6d40d5e8
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-07-31 18:03:31 +1200

    feat: convert simulator to event driven class

--- py_src/simulator/main.py
+++ py_src/simulator/main.py
@@ -1,18 +1,52 @@
 import os
+import time
+import queue
 
 import mujoco
 import mujoco.viewer
 
+from hardware_abstraction_layer import OutputLayer
+
+
 SCENE_PATH = os.path.expanduser('~/unitree_mujoco/unitree_robots/go2/scene.xml')
+
+class Simulator(OutputLayer):
+    def __init__(self):
+        self._action_queue = queue.Queue()
+
+    def connect(self):
+        model = mujoco.MjModel.from_xml_path(SCENE_PATH)                           # pyright: ignore[reportAttributeAccessIssue]
+        data = mujoco.MjData(model)                                                # pyright: ignore[reportAttributeAccessIssue]
+        with mujoco.viewer.launch_passive(model, data) as viewer:
+            while viewer.is_running():
+                step_start = time.time()
+
+                # Event Check: Has an action event arrived?
+                try:
+                    # Non-blocking check for new event
+                    new_action = self._action_queue.get_nowait()
+                    data.ctrl[:] = new_action
+                except queue.Empty:
+                    pass  # Keep previous action in data.ctrl automatically
+
+                # Physics engine steps continuously holding the last action state
+                mujoco.mj_step(model, data)                                        # pyright: ignore[reportAttributeAccessIssue]
+                viewer.sync()
+
+                # Maintain physics rate
+                time_until_next_step = model.opt.timestep - (time.time() - step_start)
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
+
+    def send_commands(self, target_angles, torques):  # TODO: Remove, to satisfy interface
+        pass
+    def send_action(self, target_angles, feedforward_torques):
+        pass
+
+    def get_low_state(self):
+        pass
 
 def main():
     print("Openning Simulator...")
-
-    model = mujoco.MjModel.from_xml_path(SCENE_PATH)                           # pyright: ignore[reportAttributeAccessIssue]
-    data = mujoco.MjData(model)                                                # pyright: ignore[reportAttributeAccessIssue]
-    with mujoco.viewer.launch_passive(model, data) as viewer:
-        while viewer.is_running():
-
-
-            mujoco.mj_step(model, data)                                        # pyright: ignore[reportAttributeAccessIssue]
-            viewer.sync()
+    sim = Simulator()
+    sim.connect()
