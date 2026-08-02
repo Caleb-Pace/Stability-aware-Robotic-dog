058278b684101b5743911bed34176800864fadda
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-08-02 16:32:04 +1200

    refactor: move controller reading logic into controller data class and add button support

--- py_src/data_structures/controller_input.py
+++ py_src/data_structures/controller_input.py
@@ -1,4 +1,7 @@
 from typing import NamedTuple
+from pygame.joystick import JoystickType
+
+JOYSTICK_DEADZONE:float = 0.05
 
 class JoyStickData(NamedTuple):
     delta_x:float
@@ -7,7 +10,35 @@ class JoyStickData(NamedTuple):
 class ControllerData():
     left_stick:JoyStickData
     right_stick:JoyStickData
+    a_btn_down:bool
+    b_btn_down:bool
+    x_btn_down:bool
+    y_btn_down:bool
 
-    def __init__(self, left_stick:JoyStickData, right_stick:JoyStickData):
-        self.left_stick = left_stick
-        self.right_stick = right_stick
+
+    # Adapted from: https://github.com/maanas444/go2-simulation/blob/main/unitreego2/controller.py
+    def _apply_deadzone(self, value):
+        """Zero out the value if it's too close to the center."""
+        if abs(value) < JOYSTICK_DEADZONE:
+            return 0.0
+        return value
+
+    def __init__(self, controller:JoystickType):
+        # Read joy sticks
+        lx = self._apply_deadzone(controller.get_axis(0))
+        ly = self._apply_deadzone(-controller.get_axis(1))
+        self.left_stick:JoyStickData = JoyStickData(delta_x=lx, delta_y=ly)
+
+        if controller.get_axis(4) == -1.0:  # Using right joysticks as 2 & 3
+            rx = self._apply_deadzone(controller.get_axis(2))
+            ry = self._apply_deadzone(-controller.get_axis(3))
+        else:                                    # Using right joysticks as 3 & 4
+            rx = self._apply_deadzone(controller.get_axis(3))
+            ry = self._apply_deadzone(-controller.get_axis(4))
+        self.right_stick:JoyStickData = JoyStickData(delta_x=rx, delta_y=ry)
+
+        # Read buttons
+        self.a_btn_down = controller.get_button(0)
+        self.b_btn_down = controller.get_button(1)
+        self.x_btn_down = controller.get_button(2)
+        self.y_btn_down = controller.get_button(3)
--- py_src/kinematic_controller/main.py
+++ py_src/kinematic_controller/main.py
@@ -15,9 +15,10 @@ def main():
     gait:Gait = TROT
 
     in_layer:InputLayer = GamepadController()
-    read_delay_ms = 100
+    read_delay_ms = 10
 
     out_layer:OutputLayer = DummyOutput()
+    out_layer.connect()
 
     engine = GaitEngine(gait, out_layer)
     clock_interval_ms    = read_delay_ms
@@ -27,13 +28,24 @@ def main():
 
     while True:
         input_data = in_layer.poll()
+        # TODO: Remove, for debugging
+        if input_data.a_btn_down:
+            print("'A' pressed; ", end="")
+        if input_data.b_btn_down:
+            print("'B' pressed; ", end="")
+        if input_data.x_btn_down:
+            print("'X' pressed; ", end="")
+        if input_data.y_btn_down:
+            print("'Y' pressed; ", end="")
+        if input_data.a_btn_down or input_data.b_btn_down or input_data.x_btn_down or input_data.y_btn_down:
+            print()
 
         input_sum = abs(input_data.left_stick.delta_x) + abs(input_data.left_stick.delta_y) + abs(input_data.right_stick.delta_x) + abs(input_data.right_stick.delta_y)
         has_input = input_sum > 0
         if has_input:
             print(f"[M ]      L ({np.round(input_data.left_stick.delta_x, 3):>6}, {np.round(input_data.left_stick.delta_y, 3):>6})    |    R ({np.round(input_data.right_stick.delta_x, 3):>6}, {np.round(input_data.right_stick.delta_y, 3):>6})")  # TODO: remove, for debugging
 
-            engine.input(input_data, 1)
+            # engine.input(input_data, 1)
 
         try:
             time.sleep(read_delay_ms / 1000)
