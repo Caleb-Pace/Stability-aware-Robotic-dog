874559cf323b3256fe8f9dc2ea861e0a7d13f235
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-08-04 00:12:32 +1200

    wip: use different indexes

--- py_src/hardware_abstraction_layer/simulator.py
+++ py_src/hardware_abstraction_layer/simulator.py
@@ -18,6 +18,16 @@ from control.pid import PIDController
 
 SCENE_PATH = os.path.expanduser('~/unitree_mujoco/unitree_robots/go2/scene.xml')
 
+# MuJoCo Indexes
+#     from: https://github.com/maanas444/go2-simulation/blob/main/unitreego2/main.py#L18
+CTRL_INDEXES = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]).flatten()
+QPOS_INDEXES = np.array([[7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]).flatten()
+QVEL_INDEXES = np.array([[6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16, 17]]).flatten()
+# TODO: Implement cleaner alternative
+# CTRL_IDX = slice(0, 12)    # 0 to 11
+# QPOS_IDX = slice(7, 19)    # 7 to 18
+# QVEL_IDX = slice(6, 18)    # 6 to 17
+
 class Simulator(OutputLayer):
     _REAL_LAY_DOWN = {
         "FR": ( 0.061,  1.236, -2.761),
@@ -37,18 +47,22 @@ class Simulator(OutputLayer):
 
         self.pids = pids
 
+    # TODO: Fix dog not responding
     def _run(self, interrupt:threading.Event):
         model = mujoco.MjModel.from_xml_path(SCENE_PATH)                           # pyright: ignore[reportAttributeAccessIssue]
         data = mujoco.MjData(model)                                                # pyright: ignore[reportAttributeAccessIssue]
 
+        # Timestep
+        DT = 0.002  # 500Hz loop
+        model.opt.timestep = DT
+
         # Persist angle targets
-        target_angles = self._REAL_LAY_DOWN
+        target_angles = self._get_target_angles_from_dict(self._REAL_LAY_DOWN)
 
         # Run simulation loop
         with mujoco.viewer.launch_passive(model, data) as viewer:
             while ( not interrupt.is_set() ) and ( viewer.is_running() ):
                 step_start = time.time()
-                dt = model.opt.timestep  # Get simulation time
 
                 # Create/Clear feedforward torque buffer
                 feedforward_torques = np.zeros(JOINT_COUNT)
@@ -64,18 +78,18 @@ class Simulator(OutputLayer):
                     pass  # Keep previous action in data.ctrl automatically
 
                 # 2. Get current state from MuJoCo
-                current_angles = data.qpos[:12]
-                current_velocities = data.qvel[:12]
+                current_angles     = data.qpos[QPOS_INDEXES]
+                current_velocities = data.qvel[QVEL_INDEXES]
 
                 # 3. Calculate applied torques using PID controller
                 applied_torques = np.empty(JOINT_COUNT)
                 zipped = zip(self.pids, target_angles, current_angles, current_velocities)
 
                 for i, (pid, target, current, vel) in enumerate(zipped):
-                    applied_torques[i] = pid.update(target, current, vel, dt)
+                    applied_torques[i] = pid.update(target, current, vel, DT)
 
                 # 4. Send the calculated applied torques to MuJoCo
-                data.ctrl[:] = (applied_torques + feedforward_torques)
+                data.ctrl[:] = (applied_torques + feedforward_torques)  # TODO: Use CTRL_INDEXES
 
                 # # TODO: Remove, for debugging
                 # if int(time.time() * 1000) % 100 == 0:
@@ -110,14 +124,18 @@ class Simulator(OutputLayer):
     def _get_feedforward_torques(self):
         return np.array(([_HIP_ABDUCTOR_TORQUE_LIMIT.maximum, _HIP_TORQUE_LIMIT.maximum, _KNEE_TORQUE_LIMIT.maximum] * LEG_COUNT), dtype=np.float64)
 
-    def _send_action_preset(self, angle_targets_dict:dict):
+    def _get_target_angles_from_dict(self, preset_dict:dict):
         # Convert dict of tuples into 1D array
-        target_angles = np.array([item for tup in angle_targets_dict.values() for item in tup])
+        return np.array([item for tup in preset_dict.values() for item in tup])
+
+    def _send_action_preset(self, angle_targets_dict:dict):
+        target_angles = self._get_target_angles_from_dict(angle_targets_dict)
 
         action = Action(target_angles, self._get_feedforward_torques())
         self.send_action(action)
 
     # Presets
+    # TODO: Add in transition between sit (lay down) and stand. (Just queue up those actions for each time step?)
     def send_stand_action(self):
         self._send_action_preset(self._REAL_STAND)
 
