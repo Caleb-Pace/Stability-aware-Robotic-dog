394de2bd7c64c4e9f8d885efabd065ea672a0531
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-08-04 22:43:27 +1200

    wip: add same batch step code as sim1

--- py_src/hardware_abstraction_layer/simulator.py
+++ py_src/hardware_abstraction_layer/simulator.py
@@ -74,75 +74,83 @@ class Simulator(OutputLayer):
 
         # Persist angle targets
         target_angles = initial_pose
+        
+        sim_time = 0.0
+        wall_origin = time.perf_counter()
 
         # Run simulation loop
         with mujoco.viewer.launch_passive(model, data) as viewer:
             while ( not interrupt.is_set() ) and ( viewer.is_running() ):
-                step_start = time.time()
-
-                # Create/Clear feedforward torque buffer
-                feedforward_torques = np.zeros(JOINT_COUNT)
-
-                # # 1. Update targets if a new action has arrived
-                # try:
-                #     # Non-blocking check for new event
-                #     new_action = self._action_queue.get_nowait()
-
-                #     target_angles       = new_action.target_angles
-                #     feedforward_torques = new_action.feedforward_torques
-                # except queue.Empty:
-                #     pass  # Keep previous action in data.ctrl automatically  # TODO: Correct comment
-
-                # # 2. Get current state from MuJoCo
-                # current_angles     = data.qpos[QPOS_INDEXES]
-                # current_velocities = data.qvel[QVEL_INDEXES]
-
-                # # 3. Calculate applied torques using PID controller
-                # applied_torques = np.empty(JOINT_COUNT)
-                # zipped = zip(self.pids, target_angles, current_angles, current_velocities)
-
-                # for i, (pid, target, angle, vel) in enumerate(zipped):
-                #     applied_torques[i] = pid.update(target, angle, vel, DT)
-
-                # # 4. Send the calculated applied torques to MuJoCo
-                # # data.ctrl[:12] = target_angles  # TODO: Use CTRL_INDEXES
-                # data.ctrl[:12] = applied_torques  # TODO: Use CTRL_INDEXES
-                # # data.ctrl[:] = (applied_torques + feedforward_torques)  # TODO: Use CTRL_INDEXES
-                for leg in range(4):
-                    leg_num = leg * 3
-                    hip_t, thigh_t, calf_t = target_angles[leg_num:(leg_num+3)]
-
-                    targets = [hip_t, thigh_t, calf_t]
-                    for j in range(3):
-                        current_p = data.qpos[QPOS_INDEXES[leg_num + j]]
-                        current_v = data.qvel[QVEL_INDEXES[leg_num + j]] 
-                        data.ctrl[leg_num + j] = self.pids[leg_num + j].update(targets[j], current_p, current_v, DT)
-
-                # # TODO: Remove, for debugging
-                # if int(time.time() * 1000) % 100 == 0:
-                #     # threshold = 1e-6
-                #     # _applied_torques = np.where(np.abs(applied_torques) < threshold, 0, applied_torques)
-                #     # _feedforward_torques = np.where(np.abs(feedforward_torques) < threshold, 0, feedforward_torques)
+                # step_start = time.time()
                 
-                #     np.set_printoptions(suppress=True, precision=6)
-                #     # print(f"{str(int(time.time() * 1000))[-6:-2]} | {_applied_torques} + {_feedforward_torques}")
-                #     print(f"[S2]  {str(int(time.time() * 1000))[-6:-2]}")
-                #     # print(f"[S2]        : {np.asarray(range(20))}")
-                #     # print(f"[S2]    d_qp: {data.qpos}")
-                #     # print(f"[S2]    d_qv: {data.qvel}")
-                #     print(f"[S2]    d_ct: {data.ctrl}")
-                #     # print(f"[S2]    ap_t: {_applied_torques}")
-                #     # print(f"[S2]    ff_t: {_feedforward_torques}")
-                #     print(f"[S2]    t_ag: {target_angles}")
+                target_sim = min(time.perf_counter() - wall_origin, sim_time + 0.050)
+                
+                while sim_time < target_sim:
+                    # # Create/Clear feedforward torque buffer
+                    # feedforward_torques = np.zeros(JOINT_COUNT)
 
-                # Physics engine steps continuously holding the last action state
-                mujoco.mj_step(model, data)                                        # pyright: ignore[reportAttributeAccessIssue]
+                    # # 1. Update targets if a new action has arrived
+                    # try:
+                    #     # Non-blocking check for new event
+                    #     new_action = self._action_queue.get_nowait()
+
+                    #     target_angles       = new_action.target_angles
+                    #     feedforward_torques = new_action.feedforward_torques
+                    # except queue.Empty:
+                    #     pass  # Keep previous action in data.ctrl automatically  # TODO: Correct comment
+
+                    # # 2. Get current state from MuJoCo
+                    # current_angles     = data.qpos[QPOS_INDEXES]
+                    # current_velocities = data.qvel[QVEL_INDEXES]
+
+                    # # 3. Calculate applied torques using PID controller
+                    # applied_torques = np.empty(JOINT_COUNT)
+                    # zipped = zip(self.pids, target_angles, current_angles, current_velocities)
+
+                    # for i, (pid, target, angle, vel) in enumerate(zipped):
+                    #     applied_torques[i] = pid.update(target, angle, vel, DT)
+
+                    # # 4. Send the calculated applied torques to MuJoCo
+                    # # data.ctrl[:12] = target_angles  # TODO: Use CTRL_INDEXES
+                    # data.ctrl[:12] = applied_torques  # TODO: Use CTRL_INDEXES
+                    # # data.ctrl[:] = (applied_torques + feedforward_torques)  # TODO: Use CTRL_INDEXES
+                    for leg in range(4):
+                        leg_num = leg * 3
+                        hip_t, thigh_t, calf_t = target_angles[leg_num:(leg_num+3)]
+
+                        targets = [hip_t, thigh_t, calf_t]
+                        for j in range(3):
+                            current_p = data.qpos[QPOS_INDEXES[leg_num + j]]
+                            current_v = data.qvel[QVEL_INDEXES[leg_num + j]] 
+                            data.ctrl[leg_num + j] = self.pids[leg_num + j].update(targets[j], current_p, current_v, DT)
+
+                    # # TODO: Remove, for debugging
+                    # if int(time.time() * 1000) % 100 == 0:
+                    #     # threshold = 1e-6
+                    #     # _applied_torques = np.where(np.abs(applied_torques) < threshold, 0, applied_torques)
+                    #     # _feedforward_torques = np.where(np.abs(feedforward_torques) < threshold, 0, feedforward_torques)
+                    
+                    #     np.set_printoptions(suppress=True, precision=6)
+                    #     # print(f"{str(int(time.time() * 1000))[-6:-2]} | {_applied_torques} + {_feedforward_torques}")
+                    #     print(f"[S2]  {str(int(time.time() * 1000))[-6:-2]}")
+                    #     # print(f"[S2]        : {np.asarray(range(20))}")
+                    #     # print(f"[S2]    d_qp: {data.qpos}")
+                    #     # print(f"[S2]    d_qv: {data.qvel}")
+                    #     print(f"[S2]    d_ct: {data.ctrl}")
+                    #     # print(f"[S2]    ap_t: {_applied_torques}")
+                    #     # print(f"[S2]    ff_t: {_feedforward_torques}")
+                    #     print(f"[S2]    t_ag: {target_angles}")
+
+                    # Physics engine steps continuously holding the last action state
+                    mujoco.mj_step(model, data)                                        # pyright: ignore[reportAttributeAccessIssue]
+                    sim_time += DT
+
                 viewer.sync()
 
-                # Maintain physics rate
-                time_until_next_step = model.opt.timestep - (time.time() - step_start)
-                if time_until_next_step > 0:
-                    time.sleep(time_until_next_step)
+                # # Maintain physics rate
+                # time_until_next_step = model.opt.timestep - (time.time() - step_start)
+                # if time_until_next_step > 0:
+                #     time.sleep(time_until_next_step)
 
     def connect(self, terminate_connection:threading.Event):
         print("[SO]  Openning simulator...")
