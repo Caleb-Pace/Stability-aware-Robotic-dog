7c1660b915705306aecb86e33da501b40668397e
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-08-12 22:39:33 +1200

    fix: nest arrays to allow for differing point amounts

--- py_src/data_structures/leg_trajectories.py
+++ py_src/data_structures/leg_trajectories.py
@@ -11,7 +11,7 @@ _GROUND_LEVEL = 0.0
 
 class LegTrajectories:
     _phase_offset:   npt.NDArray[np.float64]  # Normalised [0.0, 1.0); Movement delay
-    _control_points: npt.NDArray[np.float64]  # (Leg, Control Points, Relative Coordinates)
+    _control_points: npt.NDArray[np.object_]  # (Leg, Control Points, Relative Coordinates)
 
     foot_trajectories:       npt.NDArray[np.float64]  # (Leg, Knot/Point, Relative Coordinates)
     time_anchors:            npt.NDArray[np.float64]  # (Leg, time anchor)
@@ -23,7 +23,7 @@ class LegTrajectories:
 
     def __init__(self, sample_count:int,
                        leg_phase_offset:npt.NDArray[np.float64],
-                       leg_control_points:npt.NDArray[np.float64]):
+                       leg_control_points:npt.NDArray[np.object_]):
         # Shape checks
         #     (Leg)
         required_shape = (LEG_COUNT,)
@@ -31,47 +31,50 @@ class LegTrajectories:
             raise ValueError(f"Invalid shape {leg_phase_offset.shape}. Must be {required_shape}!")
         
         #     (Leg, Control Point, Coordinates)
-        if (leg_control_points.ndim != 3) or not (leg_control_points.shape[0] == LEG_COUNT and leg_control_points.shape[2] == 3):
-                raise ValueError(f"Invalid shape {leg_control_points.shape}. Must be ({LEG_COUNT}, *, 3)!")
-
+        if len(leg_control_points) != LEG_COUNT:
+            raise ValueError(f"Expected {LEG_COUNT} legs, got {len(leg_control_points)}.")
+        for i, leg in enumerate(leg_control_points):
+            arr = np.asarray(leg)
+            if arr.ndim != 2 or arr.shape[1] != 3:
+                raise ValueError(f"Invalid shape {arr.shape} for leg {i}. Must be (*, 3)!")
+            if len(arr) == 0:
+                raise ValueError(f"Invalid point count 0 at leg {i}. Must be at least 1!")
+        
         self._phase_offset         = leg_phase_offset
         self._control_points       = leg_control_points
     
         self.calculate_foot_trajectories(sample_count)
 
-    def _find_max_movement_horizon(self) -> float:
-        """Finds the maximum movement parametric duration among all the legs"""
-        return float(np.max(self.time_anchors[:, -1]))
-
-    def _calculate_last_time_anchors(self, alpha:float = 0.5) -> npt.NDArray[np.float64]:
+    def _calculate_last_time_anchor(self, leg:int, alpha:float = 0.5) -> npt.NDArray[np.float64]:
         """
-        Calculates the final time anchor for each leg.
+        Calculates the final time anchor for a leg.
 
         Returns:
-            npt.NDArray[np.float64]: Movement parametric duration per leg.
+            npt.NDArray[np.float64]: Movement parametric duration.
 
         Args:
             alpha: Knot spacing parameter (e.g., 0.5 for centripetal).
         """
         # Differences between consecutive control points
-        diffs = np.diff(self._control_points, axis=1)
+        diffs = np.diff(self._control_points[leg], axis=0)
         
         # Euclidean distances between consecutive control points
         distances = np.linalg.norm(diffs, axis=-1)
         
         # Sum time calculation, per leg
-        return np.sum(distances**alpha, axis=1)
+        return np.sum(distances**alpha)
 
     def _calculate_time_horizon(self, alpha:float = 0.5) -> None:
-        self.parametric_time_horizon = 0.0  # Clear previous value
-        max_movement_horizon         = self._find_max_movement_horizon()
+        movement_horizons        = np.asarray([
+            self._calculate_last_time_anchor(leg, alpha) for leg in range(LEG_COUNT)
+        ])
+        max_movement_horizon     = max(movement_horizons)
 
-        movement_horizons        = self._calculate_last_time_anchors()   # Parametric duration
         normalized_phase_offsets = self._phase_offset % 1.0
         movement_delays          = normalized_phase_offsets * max_movement_horizon   # Parametric duration
 
-        leg_durations = movement_delays + movement_horizons   # Parametric duration
-        self.parametric_time_horizon = np.max(leg_durations)  # Parametric duration of the gait
+        leg_durations = movement_delays + movement_horizons  # Parametric duration
+        self.parametric_time_horizon = max(leg_durations)    # Parametric duration of the gait
 
     def calculate_foot_trajectories(self, sample_count:int) -> None:
         ALPHA = 0.5  # Centripedal knot spacing
